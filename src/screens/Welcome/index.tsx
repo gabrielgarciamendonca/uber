b72fad5b010bd6180aa86efa24a0505eea682eb7
Gabriel Mendonça <gabriel.dev.lop@gmail.com>
2023-02-28 21:33:49 -0300

    FEAT: INCLUDING FIRST PAGE AND STYLED-COMPONENTS

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { WelcomeContainer, WelcomeTitle } from "./styles";
+
+export function Welcome() {
+    return (
+        <WelcomeContainer>
+            <WelcomeTitle>UBER COPY</WelcomeTitle>
+        </WelcomeContainer>
+    )
+}
---
+++ src/screens/Welcome/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native'
+
+export const WelcomeContainer = styled.View`
+    flex: 1;
+    background-color: #0D1724;
+    justify-content: center;
+    align-items: center;
+`
+
+export const WelcomeTitle = styled.Text`
+    font-size: 18px;
+`
